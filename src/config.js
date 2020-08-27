afb293d485be72dab23b386b5ee29af17883a63d
Suraj Verma <surajverma@rentalcars.com>
2020-08-27 18:08:36 +0100

    config file for server and mongoose

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const PORT = process.env.PORT || 8000;
+
+export const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost/restaurants';
+
+export const MONGOOSE_OPTIONS = {
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useNewUrlParser: true,
+};
