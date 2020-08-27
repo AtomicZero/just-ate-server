44a10e7622efaf0cbbf8cc1b1132d2dd302e7c65
Suraj Verma <surajverma@rentalcars.com>
2020-08-27 19:28:49 +0100

    add more seed data

--- src/data/restaurants.js
+++ src/data/restaurants.js
@@ -81,4 +81,72 @@ export default [
       },
     ],
   },
+  {
+    title: 'Taste It Italian',
+    rating: 5,
+    reviews: 295,
+    address: {
+      street: '69 Blackfriars Road',
+      city: 'Salford',
+      postcode: 'M3 7DB',
+    },
+    menu: [
+      {
+        index: 1,
+        sectionTitle: 'Pizza',
+        items: [
+          {
+            id: 'ab7b89ba-e892-11ea-adc1-0242ac120002',
+            title: 'Margherita Pizza',
+            price: 5.00,
+            description: 'Tomato sauce, mozzarella, fresh basil & olive oil',
+            warningIcons: ['veg'],
+          },
+          {
+            id: 'af620c02-e892-11ea-adc1-0242ac120002',
+            title: 'Napoli Pizza',
+            price: 5.00,
+            description: 'Tomato sauce, anchovies, garlic,oregano, olive oil',
+          },
+          {
+            id: 'b28096a6-e892-11ea-adc1-0242ac120002',
+            title: 'Diavola Pizza',
+            price: 5.50,
+            description: 'Tomato sauce, mozzarella, pepperoni & olive oil',
+            warningIcons: ['spicy1'],
+          },
+        ],
+      },
+      {
+        index: 2,
+        sectionTitle: 'Appetisers',
+        items: [
+          {
+            id: '01d16942-e893-11ea-adc1-0242ac120002',
+            title: 'Hand Cut Chips',
+            price: 2.50,
+          },
+          {
+            id: '05ce5f82-e893-11ea-adc1-0242ac120002',
+            title: 'Mozzarella Sticks',
+            price: 2.00,
+            description: 'Deep fried mozzarella sticks',
+          },
+          {
+            id: '090d6f76-e893-11ea-adc1-0242ac120002',
+            title: 'Beef Ragu Arancini - 1 Piece',
+            price: 3.00,
+            description: 'Deep fried rice balls with beef ragu, mozzarella, parmesan & saffron',
+          },
+          {
+            id: '0d5a7e7a-e893-11ea-adc1-0242ac120002',
+            title: 'Mushroom Arancini - 1 Piece',
+            price: 3.00,
+            description: 'Deep fried rice balls with mushrooms, mozzarella, parmesan & saffron',
+            warningIcons: ['veg'],
+          },
+        ],
+      },
+    ],
+  },
 ];
