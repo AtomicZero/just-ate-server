79a5d958c21c8de45ed2098db65ab4e8379af296
Suraj Verma <surajverma@rentalcars.com>
2020-08-27 19:19:19 +0100

    update db name to just-ate-db

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 8000;
 
-export const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost/restaurants';
+export const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost/just-ate-db';
 
 export const MONGOOSE_OPTIONS = {
   useUnifiedTopology: true,
