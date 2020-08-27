e77ac2f8b5bcbd017f2da7d5a43718677a96b3b1
Suraj Verma <surajverma@rentalcars.com>
2020-08-27 19:19:41 +0100

    seed data file

---
+++ src/data/restaurants.js
@@ -0,0 +1,84 @@
+export default [
+  {
+    title: 'Very Peri Mcr',
+    rating: 4.5,
+    reviews: 144,
+    address: {
+      street: 'Unit 3, 365 Chapel Street',
+      city: 'Salford',
+      postcode: 'M3 5JT',
+    },
+    menu: [
+      {
+        index: 1,
+        sectionTitle: 'Sizzling Steaks',
+        sectionDescription: 'All steaks are served with either peppercorn or mushroom sauce and 2 sides. Choose a delicious side from rice, mash, fries and peri fried.',
+        items: [
+          {
+            id: '93841900-e890-11ea-adc1-0242ac120002',
+            title: 'Chicken Steak',
+            price: 8.99,
+            description: 'Shredded  chicken steak lightly seasoned with grilled onions & peppers',
+          },
+          {
+            id: '9d1edcb6-e890-11ea-adc1-0242ac120002',
+            title: 'Sirloin Steak',
+            price: 9.99,
+            description: 'Thin shredded sirloin steak lightly seasoned with grilled onions & peppers',
+          },
+          {
+            id: '9fc8c90e-e890-11ea-adc1-0242ac120002',
+            title: 'Mix Steak',
+            price: 11.49,
+            description: 'Chicken shredded steak & sirloin shredded steak, grilled onions & peppers',
+          },
+        ],
+      },
+      {
+        index: 2,
+        sectionTitle: 'Grilled Burgers',
+        sectionDescription: 'Make it a Meal comes with fries and drink.',
+        items: [
+          {
+            id: 'a4698f66-e890-11ea-adc1-0242ac120002',
+            title: 'Cheese Burger',
+            price: 2.99,
+            description: '4oz juicy beef patty, cheese slice, onions, lettuce & ketchup',
+          },
+          {
+            id: 'a74d951a-e890-11ea-adc1-0242ac120002',
+            title: 'Supreme Cheese Burger',
+            price: 3.99,
+            description: '6oz juicy beef petty, cheese slice, onions, lettuce & ketchup',
+          },
+          {
+            id: 'aae3e5b2-e890-11ea-adc1-0242ac120002',
+            title: 'Chicken Fillet Burger',
+            price: 3.99,
+            description: 'Chicken fillet with lettuce and mayo',
+          },
+          {
+            id: 'ade29812-e890-11ea-adc1-0242ac120002',
+            title: 'Peri Peri Burger',
+            price: 3.99,
+            description: 'Succulent peri peri chicken fillet with lettuce and spicy mayo',
+            warningIcons: ['spicy1'],
+          },
+          {
+            id: 'b1802430-e890-11ea-adc1-0242ac120002',
+            title: 'Peri Peri Boom Burger',
+            price: 4.49,
+            description: 'Succulent peri peri chicken fillet, cheese slice, jalapeno cheese bites, lettuce and spicy mayo',
+            warningIcons: ['spicy1'],
+          },
+          {
+            id: 'b433b502-e890-11ea-adc1-0242ac120002',
+            title: 'Veg Burger',
+            price: 2.99,
+            description: 'With cheese, onions and lettuce',
+          },
+        ],
+      },
+    ],
+  },
+];
