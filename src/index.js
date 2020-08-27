8ccb72655c8a8cbf22967d0952a0c27638db63a7
Suraj Verma <surajverma@rentalcars.com>
2020-08-27 18:08:04 +0100

    initial index file with express and mongoose

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-import { greet } from './greet';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-const result = greet('Bob');
+import { PORT, DB_URI, MONGOOSE_OPTIONS } from './config';
 
-console.log(result);
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+mongoose.connect(DB_URI, MONGOOSE_OPTIONS);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on: http://localhost:${PORT}`);
+});
